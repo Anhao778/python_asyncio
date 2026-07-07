c5fb427d361476c4f9cafa6aae3c7834681d2891
Zhenyang <3244817161@qq.com>
2026-07-07 13:45:19 +0800

    yield

--- 1.1_greenlet.py
+++ 1.1_greenlet.py
@@ -2,17 +2,17 @@ import greenlet
 
 def func1():
     print(123)
-    g2.switch()
+    g2.switch()# 切换到g2
     print(456)
-    g2.switch()
+    g2.switch()# 切换到g2
 
 def func2():
     print(789)
-    g1.switch()
+    g1.switch()# 切换到g1
     print(101112)
-    g1.switch()
+    g1.switch()# 切换到g1   
 
 g1 = greenlet.greenlet(func1)
 g2 = greenlet.greenlet(func2)
 
-g1.switch()
+g1.switch() # step1 do g1
